1cf054cc24544b120609cbff426f0242d587c84d
Nima Bayat <bayatnima343@gmail.com>
2024-06-19 07:43:17 -0400

    add product admin.

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from .models import Product,ProductPrice,ProductOption,Question,Answer,Image,Comment,Category
+
+
 
 # Register your models here.
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'en_name', 'name', 'category']
+    list_filter = ['category']
+    search_fields = ['en_name', 'name']
+
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'slug', 'parent']
+    list_filter = ['parent']
+    search_fields = ['name','description']
+    fieldsets = (
+        ('details',{
+            'fields':('name','slug',
+                      'parent',
+                      'description')
+        }),
+        ('images', {
+            'fields': ('icon', 'image'),
+        }),
+    )
+
+
+
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    pass
+
+
+
+
