8dcd5828fcca97c9ea380235850c186718afb114
Nima Bayat <bayatnima343@gmail.com>
2024-09-10 11:27:05 -0400

    urls improved.

--- digikala/urls.py
+++ digikala/urls.py
@@ -23,7 +23,7 @@ from django.conf import settings
 urlpatterns = ([
     path('admin/', admin.site.urls),
     path('', include('products.urls')),
-    path('products/', include('products.urls'))
+    path('product/', include('products.urls'))
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) +
                static(settings.STATIC_URL, document_root = settings.STATIC_ROOT))
 
--- products/urls.py
+++ products/urls.py
@@ -2,7 +2,7 @@ from .views import product_list_view , product_single_view
 from django.urls import path
 
 urlpatterns = [
-    path('', product_list_view),
-    path('<int:product_id>/', product_single_view),
+    path('', product_list_view , name='product-list'),
+    path('<int:product_id>/', product_single_view , name = 'product-single'),
 
 ]
