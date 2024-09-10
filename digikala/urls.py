6cfd87fe0e64e47e598d9ba4cad81281eaf01e27
Nima Bayat <bayatnima343@gmail.com>
2024-09-10 07:51:57 -0400

    stylesheet added.

--- digikala/urls.py
+++ digikala/urls.py
@@ -20,10 +20,11 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 
-urlpatterns = [
+urlpatterns = ([
     path('admin/', admin.site.urls),
     path('', include('products.urls'))
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) /
-    + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) +
+               static(settings.STATIC_URL, document_root = settings.STATIC_ROOT))
+
 
 
